acd2209ef5b5f7ea8fc00cac30e2ae2f8de50103
김태현 Ranker팀 <hyeonkim@sktelecom.com>
2023-12-07 04:28:51 +0900

    update gui

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QListWidgetItem, QVBoxLayout, QPushButton, QTextEdit, QLineEdit
+from PyQt5.QtGui import QPalette, QColor, QIcon
+from PyQt5.QtCore import Qt, QSize
 from client_module import VoiceCommunication  # VoiceCommunication 모듈은 실제 모듈명으로 변경해야 합니다.
 from threading import Thread
 from time import sleep
@@ -20,22 +22,74 @@ class ChatWindow(QWidget):
         
 
     def initUI(self):
+        # 전체 배경과 텍스트 색상 설정
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(18,18,18)) # 배경색
+        palette.setColor(QPalette.WindowText, Qt.white) # 텍스트 색상
+        self.setPalette(palette)
+
         layout = QVBoxLayout()
 
         if self.typevoice:
-            self.text_area = QTextEdit()
-            self.text_area.setReadOnly(True)
-            layout.addWidget(self.text_area)
+            self.messageList = QListWidget()
+            self.messageList.setStyleSheet("""
+                QListWidget {
+                    background-color: #121212;
+                    color: white;
+                    font-size: 16px;
+                    border: none;
+                }
+            """)
+            layout.addWidget(self.messageList)
 
             self.input_line = QLineEdit()
+            self.input_line.setAlignment(Qt.AlignRight)  # 입력 필드의 텍스트를 우측 정렬로 설정
+            self.input_line.setStyleSheet("""
+                QLineEdit {
+                    background-color: #121212;
+                    color: white;
+                    border: 2px solid #474747;
+                    border-radius: 20px;
+                    padding: 5px 15px;
+                    font-size: 16px;
+                }
+            """)
             self.input_line.setPlaceholderText("메시지를 입력하세요...")
             self.input_line.textEdited.connect(self.send_message_by_space)
             self.input_line.returnPressed.connect(self.send_message)  # 엔터 키 이벤트 연결
             layout.addWidget(self.input_line)
 
-            send_button = QPushButton('보내기')
-            send_button.clicked.connect(self.send_message)
-            layout.addWidget(send_button)
+            # 통화 종료 버튼 설정
+            hangupButton = QPushButton()
+            hangupButton.setIcon(QIcon('hangup.png'))  # 이미지 파일 경로를 지정해야 합니다
+            hangupButton.setIconSize(QSize(80, 40))  # 이미지 크기 설정
+            hangupButton.setStyleSheet("""
+                QPushButton {
+                    background-color: #D32F2F;
+                    color: white;
+                    font-weight: bold;
+                    border-radius: 20px; /* 모서리 둥글게 처리 */
+                    font-size: 16px;
+                }
+                QPushButton:hover {
+                    background-color: #FF5252;
+                }
+                QPushButton:pressed {
+                    background-color: #C62828;
+                }
+            """)
+            hangupButton.setFixedSize(80, 40)  # 버튼 크기 설정
+            
+            # 버튼을 수평으로 가운데 정렬
+            buttonContainer = QWidget(self)
+            buttonLayout = QVBoxLayout(buttonContainer)
+            buttonLayout.addWidget(hangupButton)
+            buttonLayout.setAlignment(Qt.AlignCenter)
+            buttonContainer.setLayout(buttonLayout)
+            layout.addWidget(buttonContainer, alignment=Qt.AlignCenter)
+            
+            hangupButton.clicked.connect(self.hangupCall)
 
             self.setLayout(layout)
             self.setWindowTitle(f"{self.source_name}의 채팅방 (Type-Voice 사용)")
@@ -51,7 +105,12 @@ class ChatWindow(QWidget):
         while True:
             sleep(0.01)
             text = self.output_text_q.get()
-            self.text_area.append(f"{self.dest_name}: {text}")
+
+            # 메시지 리스트에 아이템 추가
+            item = QListWidgetItem(text)
+            item.setForeground(QColor('white'))  # 글자색 설정
+            item.setTextAlignment(Qt.AlignLeft)
+            self.messageList.addItem(item)
 
     def send_message_by_space(self):
         # 입력 필드에서 텍스트를 가져와 출력 필드에 설정
@@ -68,8 +127,16 @@ class ChatWindow(QWidget):
                 self.input_text_q.put(text.strip().split(' ')[-1])
         
         if text:
-            self.text_area.append(f"{self.source_name}: {text}")
+            # 메시지 리스트에 아이템 추가
+            item = QListWidgetItem(text)
+            item.setForeground(QColor('white'))  # 글자색 설정
+            item.setTextAlignment(Qt.AlignRight)
+            self.messageList.addItem(item)
             self.input_line.clear()
+    
+    def hangupCall(self):
+        # 통화 종료 로직 (여기서는 단순히 앱을 종료합니다)
+        self.close()
 
 def main():
     app = QApplication(sys.argv)
@@ -90,8 +157,9 @@ def main():
         vc_thread = Thread(target=vc.run)
         vc_thread.start()
         
-        ex = ChatWindow(source_name, dest_name, typevoice, input_text_q, output_text_q)
-        ex.show()
+        chat = ChatWindow(source_name, dest_name, typevoice, input_text_q, output_text_q)
+        chat.resize(800, 600)  # 창 크기 설정
+        chat.show()
 
     except KeyboardInterrupt:
         print("keyboard interrupt")
