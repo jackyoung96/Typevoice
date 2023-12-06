054e93122e70d312ba1ddc3131b53f44d9e08f4a
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2023-12-07 01:59:59 +0900

    spacebar

--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QVBoxLayout
+from PyQt5.QtCore import Qt, pyqtSignal
 from client_module import VoiceCommunication  # VoiceCommunication 모듈은 실제 모듈명으로 변경해야 합니다.
 from threading import Thread
 from time import sleep
-import asyncio
 
 class MyApp(QWidget):
     def __init__(self, typevoice, input_text_q, output_text_q):
@@ -19,6 +19,7 @@ class MyApp(QWidget):
         if self.typevoice:
             self.input_line = QLineEdit()  
             self.input_line.setPlaceholderText("여기에 텍스트 입력")
+            self.input_line.textEdited.connect(self.on_space_pressed)
             self.input_line.returnPressed.connect(self.on_return_pressed)
 
             # 출력 필드 생성
@@ -38,21 +39,28 @@ class MyApp(QWidget):
         self.output_text_q = output_text_q
         
         if typevoice:
-            tts_thread = Thread(target=self.get_tts_message, daemon=True)
-            tts_thread.start()
+            stt_thread = Thread(target=self.get_stt_message, daemon=True)
+            stt_thread.start()
         
-    def get_tts_message(self):
+    def get_stt_message(self):
         while True:
             sleep(0.01)
             text = self.output_text_q.get()
-            self.output_line.setText(text)
-            
+            self.output_line.setText(self.output_line.text().strip() + ' ' + text.strip())
 
-    def on_return_pressed(self):
+    def on_space_pressed(self):
         # 입력 필드에서 텍스트를 가져와 출력 필드에 설정
         input_text = self.input_line.text()
-        self.input_text_q.put(input_text)
-        # self.output_line.setText(input_text)
+        if input_text.endswith(' '):
+            if input_text.strip():
+                self.input_text_q.put(input_text.strip().split(' ')[-1])
+        
+    def on_return_pressed(self):
+        # 입력 필드에서 텍스트를 가져와 출력 필드에 설정
+        print("return pressed", flush=True)
+        input_text = self.input_line.text()
+        self.input_text_q.put(input_text.strip().split(' ')[-1])
+        self.input_line.setText('')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
